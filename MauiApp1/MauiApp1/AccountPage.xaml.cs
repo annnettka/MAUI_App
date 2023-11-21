f76ca0b69d7808c751f796ec4851a21a28ac6402
Anna Pivtoranis <annap@funtico.com>
2023-11-21 10:11:22 +0200

    init

---
+++ MauiApp1/MauiApp1/AccountPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class AccountPage : ContentPage
+    {
+        public AccountPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Створення нової сторінки для переходу
+            AccountPage accountPage = new AccountPage();
+
+            // Виклик методу Navigation для переходу на другу сторінку
+            await Navigation.PushAsync(accountPage);
+        }
+    }
+}
---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"{count} раз";
+            else
+                CounterBtn.Text = $"{count} - разів";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+
+            AccountPage accountPage = new AccountPage();
+            Navigation.PushAsync(accountPage);
+        }
+    }
+}
+
